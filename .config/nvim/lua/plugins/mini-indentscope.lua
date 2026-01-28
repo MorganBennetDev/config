ff140b0d254b6204c28eb30786c39b693ae2bf19
Morgan Bennet <98787614+MorganBennetDev@users.noreply.github.com>
2026-01-27 21:41:41 -0700

    Neovim/Lazyvim config

---
+++ .config/nvim/lua/plugins/mini-indentscope.lua
@@ -0,0 +1,14 @@
+return {
+    "nvim-mini/mini.indentscope",
+    version = false, -- wait till new 0.7.0 release to put it back on semver
+    event = "LazyFile",
+    opts = {
+        -- symbol = "▏",
+        symbol = "│",
+        options = { try_as_border = true },
+
+        draw = {
+            animation = require("mini.indentscope").gen_animation.none(),
+        },
+    },
+}
